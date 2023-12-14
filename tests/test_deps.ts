e2f1e99eb83301f12079677769fe1b9d4bf891f7
vectronic <vectronic@users.noreply.github.com>
2023-12-14 01:09:57 +0000

    chore: update dependencies

--- tests/test_deps.ts
+++ tests/test_deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.208.0/testing/asserts.ts";
-export * as path from "https://deno.land/std@0.208.0/path/mod.ts";
+export * from "https://deno.land/std@0.209.0/testing/asserts.ts";
+export * as path from "https://deno.land/std@0.209.0/path/mod.ts";
